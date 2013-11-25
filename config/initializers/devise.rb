b00dce7d4d727e03a5082983bc3233ea1105b70b
Kevin Wanek <k@dmcy.us>
2013-11-24 22:15:14 -0700

    added mongoid config, updated devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -228,7 +228,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: 'email'
-  config.omniauth :twitter, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET']
+  config.omniauth :twitter, ENV['TWITTER_CONSUMER'], ENV['TWITTER_SECRET']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
